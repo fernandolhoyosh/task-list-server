0dc2c5a2f82e5c1e1f01dd558a92569ed629eefa
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-08-31 12:48:10 -0500

    response GET tasks pending

--- routers/list-view-router.js
+++ routers/list-view-router.js
@@ -8,12 +8,13 @@ const tasksCompleted = serviceTasks.tasksCompleted;
 const tasksIncomplete = serviceTasks.tasksPending;
 
 router.get('/completed', (req, res) => {
-
+    console.log(`Enviando tareas completas al cliente`);
+    res.status(200).json(tasksCompleted(taskList));
 });
 
 router.get('/pending', (req, res) => {
     console.log(`Enviando tareas pendientes al cliente`);
-    res.status(200).json(tasksIncomplete(taskList))
+    res.status(200).json(tasksIncomplete(taskList));
 });
 
 module.exports = router;
