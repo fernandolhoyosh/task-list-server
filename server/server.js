e4e7770bdfaeb93bd0cfed5ae785bd6f29c591ba
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-08-31 01:59:09 -0500

    Main server express

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const listViewRouter = require('../routers/list-view-router');
+const listEditRouter = require('../routers/list-edit-router');
+
+const app = express();
+const port = 3000;
+
+app.use('/tasks', listViewRouter);
+app.use('/tasks', listEditRouter);
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to To-Do Server');
+});
+
+app.listen(port, ()=> {
+    console.log(`Server online on port:${port}`)
+});
