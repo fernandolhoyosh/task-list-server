00c8899e08025e613bcff1339f8fddb063752ea9
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-08-31 02:00:30 -0500

    task list server

--- server/app.js
+++ server/app.js
@@ -1,17 +1,21 @@
-// Importo file System
-const fs = require("fs");
-// Importo Express
-const express = require("express");
+
+const fs = require("node:fs");          // Importo el modulo File System
+const patch = require('node:path');     // Importo el modulo Patch de Node
+const express = require("express");     // Importo Express
+
 const app = express();
 const port = 3000;
+
+ //Establezco la ruta del archivo JSON que contiene la lista de tareas
+ const filePatch = patch.join(__dirname,'..','data','tasks.json');
 
 // respuesta del servidor al recibir el metodo GET y ruta raiz
 app.get("/", (req, res) => {
     // Con file system cargo el archivo JSON con la lista de tareas para enviar al cliente
-  fs.readFile("data/tasks.json", "utf-8", (error, data) => {
+  fs.readFile(filePatch, "utf-8", (error, data) => {
     // Si el archivo tiene un error al cargar el archivo JSON imprime la respuesta de error al cliente
     if (error) {
-      const messageError = {mensaje: "Error al leer el archivo JSON", error}
+      const messageError = {message: "Error al leer el archivo JSON. Ruta no encontrada", error}
       res.status(404);
       res.send(messageError);
     } else {
@@ -24,7 +28,7 @@ app.get("/", (req, res) => {
         // si no puede convertir a JSON el archivo cargado imprime el error y lo muestra al cliente
         console.error(err);
         const messageError = err.toString();
-        res.status(404);
+        res.status(500);
         res.send(messageError);
       }
     }
