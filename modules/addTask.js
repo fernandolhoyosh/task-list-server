6133ec20c277f14df9fe251c2b7b290100e42223
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-08-31 18:55:44 -0500

    service  add task

--- modules/addTask.js
+++ modules/addTask.js
@@ -1,5 +1,17 @@
-const addTask = (taskslist, newTask) => {
-    taskslist.push(newTask);
+const addTask = (taskslist, taskName) => {
+    return new Promise((resolve, reject) => {
+        const newTask = {
+            description: taskName,
+            completed: false,
+        }
+        taskslist.push(newTask);
+        resolve({
+            status:"OK",
+            code: 201,
+            message: "Task successfully added",
+            newTask,
+        });
+    });
 }
 
 module.exports = addTask;
