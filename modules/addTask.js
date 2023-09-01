ac47b29a0b21da6a3cdd7c5e6cc7be5146886fa0
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-01 15:49:04 -0500

    added function autoID

--- modules/addTask.js
+++ modules/addTask.js
@@ -1,9 +1,13 @@
 const addTask = (taskslist, taskName) => {
     return new Promise((resolve, reject) => {
+        
+        const idTask = taskslist.length === 0 ? 1 : taskslist[taskslist.length-1].id + 1;
+
         const newTask = {
+            id: idTask,
             description: taskName,
             completed: false,
-        }
+        }    
         taskslist.push(newTask);
         resolve({
             status:"OK",
