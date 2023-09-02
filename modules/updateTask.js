3546aaf4b917960aa06a2661bd0a9084916c3312
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-02 00:54:02 -0500

    update response promise to JSON

--- modules/updateTask.js
+++ modules/updateTask.js
@@ -2,11 +2,21 @@ const updateTask = (taskList, idTask, newTaskName, status) => {
     return new Promise((resolve, reject) => {
        const taskUpdate = taskList.find(task => task.id === idTask);  //Referencia de memoria del array original
        if (!taskUpdate) {
-        reject('tarea no encontrada');
+        reject({
+            status: "error",
+            code: 404,
+            message: "task ID not found",
+            value: idTask
+        });
        } else {
         newTaskName ? taskUpdate.description = newTaskName : taskUpdate.description;
         status ? taskUpdate.completed = status === "true" : taskUpdate.completed;
-        resolve('tarea actualizada');
+        resolve({
+            status: "OK",
+            code: 200,
+            message: "task successfully updated",
+            taskUpdate
+        });
        }
     })
 }
