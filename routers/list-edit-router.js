0a9a64b8ce9dec8a8b9bdd430ff584a6b3e5fc36
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-03 16:44:11 -0500

    update response status code dinamic

--- routers/list-edit-router.js
+++ routers/list-edit-router.js
@@ -11,28 +11,28 @@ router.post("/add/:task", (req, res) => {
   const taskName = req.params.task;
   addTask(taskList, taskName)
     .then((response) => {
-      res.status(201).send(response);
+      res.status(response.code).send(response);
       console.log("New task added:", response.newTask);
       console.log(taskList);
     })
     .catch((error) => {
-      res.status(400).send(`error 400`);
+      res.status(error.code).json(error);
     });
 });
 
 router.delete("/delete/:id", (req, res) => {
   const idTask = req.params.id;
   deleteTask(taskList, idTask)
-    .then((response) => res.status(200).send(response))
-    .catch((error) => res.status(404).send(error));
+    .then((response) => res.status(response.code).send(response))
+    .catch((error) => res.status(error.code).send(error));
 });
 
 router.put("/update/:id", (req, res) => {
     const idTask = req.params.id;
     const {name, completed} = req.query;
     updateTask(taskList, idTask, name, completed)
-        .then(response => res.status(200).send(response))
-        .catch(error => res.status(404).send(error));
+        .then(response => res.status(response.code).send(response))
+        .catch(error => res.status(error.code).send(error));
 });
 
 module.exports = router;
