c3104a6395765b19c867a0df8382500a741ecba0
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-03 16:43:32 -0500

    add validations to add tasks

--- modules/addTask.js
+++ modules/addTask.js
@@ -1,20 +1,40 @@
 const addTask = (taskslist, taskName) => {
     return new Promise((resolve, reject) => {
-        
-        const idTask = String(taskslist.length === 0 ? 1 : parseInt(taskslist[taskslist.length-1].id) + 1);
-        
-        const newTask = {
-            id: idTask,
-            description: taskName,
-            completed: false,
-        }    
-        taskslist.push(newTask);
-        resolve({
-            status:"OK",
-            code: 201,
-            message: "Task successfully added",
-            newTask,
-        });
+        const taskNameTrim = taskName.trim();
+        if (!taskNameTrim) {
+            return reject({
+                status:"bad request",
+                code: 400,
+                message: "the task cannot be empty",
+                value: taskName,
+            });
+        }
+            
+            const taskFilter = taskslist.find(task => task.description === taskNameTrim);
+        if (taskFilter) {
+            reject({
+                status:"conflict found",
+                code: 409,
+                message: "the task you entered already exists in the list",
+                taskFilter,
+            });
+        } else {
+            
+            const idTask = String(taskslist.length === 0 ? 1 : parseInt(taskslist[taskslist.length-1].id) + 1);
+            
+            const newTask = {
+                id: idTask,
+                description: taskNameTrim,
+                completed: false,
+            }    
+            taskslist.push(newTask);
+            resolve({
+                status:"OK",
+                code: 201,
+                message: "Task successfully added",
+                newTask,
+            });
+        }
     });
 }
 
