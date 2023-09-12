934d55ed0d968013cca8e9cb1dabd0e8a6a2b536
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-12 11:41:12 -0500

    add middle validate http to use app

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const listViewRouter = require('../routers/list-view-router');
-const listEditRouter = require('../routers/list-edit-router');
-
 const app = express();
 const port = 3000;
 
+const morgan = require('morgan');
+const path = require('node:path');
+
+const {httpMethods} = require(path.join(__dirname,'..','middlewares','httpMethods.js'));
+const listViewRouter = require('../routers/list-view-router');
+const listEditRouter = require('../routers/list-edit-router');
+
+
+app.use(morgan('dev')); //middleware para ver las solicitudes de los clientes
+app.use(httpMethods);  // middleware para controlar metodos http validos en el server
 app.use(express.json());
 app.use('/tasks', listViewRouter);
 app.use('/tasks', listEditRouter);
