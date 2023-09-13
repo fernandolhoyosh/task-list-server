03f9e8fa84a2963f9d4bc89bef7ff24a0dfa1396
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-13 02:23:08 -0500

    optimize response JSON

--- modules/updateTask.js
+++ modules/updateTask.js
@@ -1,24 +1,16 @@
+const response = require('../resources/messages/responseUpdate')
+
 const updateTask = (taskList, idTask, newTaskName, status) => {
   return new Promise((resolve, reject) => {
     const taskUpdate = taskList.find((task) => task.id === idTask); //Referencia de memoria del array original
     if (!taskUpdate) {
-      reject({
-        status: "error",
-        code: 404,
-        message: "task ID not found",
-        value: idTask,
-      });
+      reject({...response.errorNotFound, value: idTask});
     } else {
       newTaskName ? (taskUpdate.description = newTaskName.trim()) : taskUpdate.description;
       if (status === false || status === true) {
       taskUpdate.completed = status;
       } else  taskUpdate.completed;
-      resolve({
-        status: "OK",
-        code: 200,
-        message: "task successfully updated",
-        taskUpdate,
-      });
+      resolve({...response.success, taskUpdate});
     }
   });
 };
