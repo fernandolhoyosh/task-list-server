1ef342eceb8be876b37caaff3656d8cd80353731
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-12 20:08:13 -0500

    update code logic

--- routers/list-view-router.js
+++ routers/list-view-router.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+
 const taskList = require('../data/tasks');
 
-const serviceTasks = require('../modules/showTasks');
-const tasksCompleted = serviceTasks.tasksCompleted;
-const tasksIncomplete = serviceTasks.tasksPending;
+const {validateGetParams} = require('../middlewares/validateGetParams');
+const {tasksCompleted, tasksPending} = require('../modules/showTasks');
 
-router.get('/completed', (req, res) => {
-    console.log(`Enviando tareas completas al cliente`);
-    res.status(200).json(tasksCompleted(taskList));
+/* router.use(validateGetParams); */
+
+router.get('/show/:key', validateGetParams, (req, res) => {
+    /* console.log(req.params.key); */
+    /* console.log(`Enviando tareas completas al cliente`);
+    res.status(200).json(tasksCompleted(taskList)); */
 });
 
-router.get('/pending', (req, res) => {
+router.get('/show/:pending', validateGetParams, (req, res) => {
+    req.params.pending;
     console.log(`Enviando tareas pendientes al cliente`);
-    res.status(200).json(tasksIncomplete(taskList));
-});
+    res.status(200).json(tasksPending(taskList));
+}); 
 
-router.get('/all', (req, res) => {
+router.get('/show/:all', validateGetParams, (req, res) => {
+    req.params.all;
     console.log(`Enviando lista de tareas al cliente`);
     res.status(200).json(taskList);
 });
