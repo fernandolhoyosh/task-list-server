e5cb32fb7bad7546e8de49fa13f8a882b346a7ff
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-12 20:07:35 -0500

    comento morgan

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const morgan = require('morgan');
+/* const morgan = require('morgan'); */
 const path = require('node:path');
 
 const {httpMethods} = require(path.join(__dirname,'..','middlewares','httpMethods.js'));
@@ -10,7 +10,8 @@ const listViewRouter = require('../routers/list-view-router');
 const listEditRouter = require('../routers/list-edit-router');
 
 
-app.use(morgan('dev')); //middleware para ver las solicitudes de los clientes
+/* app.use(morgan('dev')); //middleware para ver las solicitudes de los clientes */
+
 app.use(httpMethods);  // middleware para controlar metodos http validos en el server
 app.use(express.json());
 app.use('/tasks', listViewRouter);
@@ -19,6 +20,11 @@ app.use('/tasks', listEditRouter);
 app.get('/', (req, res) => {
     res.status(200).send('Welcome to To-Do Server');
 });
+
+//middleware para manejar rutas no validas en el servidor
+app.use((req, res) => {
+    res.status(404).json({status:"error",code:404, message:"resource not found"});
+})
 
 app.listen(port, ()=> {
     console.log(`Server online on port:${port}`)
