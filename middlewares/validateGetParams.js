a04b5cadada3e9f385d6a8d851df8257ce62f0a1
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-13 01:28:19 -0500

    add return response middle

--- middlewares/validateGetParams.js
+++ middlewares/validateGetParams.js
@@ -1,9 +1,10 @@
 const validateGetParams = (req, res, next) => {
+    console.log("PARAMS DESDE MIDDLE GET",req.params)
     const getParams = ['completed','pending','all'];
     const param = req.params.key;
 
     if (!getParams.includes(param)) {
-        res.status(400).json({status:"error", code:400, message:"invalid parameters"});
+        return res.status(400).json({status:"error", code:400, message:"invalid parameters"});
     }
     next();
 }
