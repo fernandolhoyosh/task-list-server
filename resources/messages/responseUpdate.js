63c12089c7fe88303040f5f7917f63f7cfdab612
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-13 02:23:55 -0500

    add error messages

---
+++ resources/messages/responseUpdate.js
@@ -0,0 +1,14 @@
+const data = {
+    errorNotFound: {
+      status: "error",
+      code: 404,
+      message: "task ID not found",
+    }, 
+    success: {
+      status: "OK",
+      code: 200,
+      message: "task successfully updated"
+    }
+};
+
+module.exports = data;
