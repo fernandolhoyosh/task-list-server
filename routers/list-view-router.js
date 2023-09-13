eb23bb0847d10e197184b6fd2d0582dae61116b9
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-13 01:32:20 -0500

    optimize endpoint GET task

--- routers/list-view-router.js
+++ routers/list-view-router.js
@@ -1,30 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-
 const taskList = require('../data/tasks');
 
 const {validateGetParams} = require('../middlewares/validateGetParams');
-const {tasksCompleted, tasksPending} = require('../modules/showTasks');
+const {showTasks} = require('../modules/showTasks');
 
 /* router.use(validateGetParams); */
 
 router.get('/show/:key', validateGetParams, (req, res) => {
-    /* console.log(req.params.key); */
-    /* console.log(`Enviando tareas completas al cliente`);
-    res.status(200).json(tasksCompleted(taskList)); */
-});
-
-router.get('/show/:pending', validateGetParams, (req, res) => {
-    req.params.pending;
-    console.log(`Enviando tareas pendientes al cliente`);
-    res.status(200).json(tasksPending(taskList));
-}); 
-
-router.get('/show/:all', validateGetParams, (req, res) => {
-    req.params.all;
-    console.log(`Enviando lista de tareas al cliente`);
-    res.status(200).json(taskList);
+    const parameter = req.params.key;
+    console.log('Sending tasks to the client');
+    res.status(200).json(showTasks(taskList, parameter));
 });
 
 module.exports = router;
