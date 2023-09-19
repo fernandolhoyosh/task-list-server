532a17da4433cdd3bcfd749e3ca48401f52f16a0
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-19 13:00:28 -0500

    add propierty password

--- data/users.js
+++ data/users.js
@@ -1,8 +1,8 @@
 const users = [
-  { username: "fernando", email: "fernandolhoyosh@gmail.com", rol: "admin" },
-  { username: "adaschool", email: "adalovelace@adaschool.org", rol: "user" },
-  { username: "gunsnroses", email: "gnr@rock.us", rol: "user" },
-  { username: "administrator", email: "admin@developer.net", rol: "admin" },
+  { username: "fernando", password: "Hoyos2023", email: "fernandolhoyosh@gmail.com", rol: "admin" },
+  { username: "adaschool", password: "AdaSchool123", email: "adalovelace@adaschool.org", rol: "user" },
+  { username: "gunsnroses", password: "Axl&Slash", email: "gnr@rock.us", rol: "user" },
+  { username: "administrator", password: "root", email: "admin@developer.net", rol: "admin" }
 ];
 
 module.exports = users;
