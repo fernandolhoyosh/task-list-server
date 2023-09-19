efad0515d32443392a220f7e498d98e0596eea08
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-19 12:58:13 -0500

    route login response token

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-    
+const users = require('../data/users');
+const {loginUser} = require('../middlewares/auth/loginUser');
+
+router.post('/login', loginUser, (req, res) => {
+    const token = req.accessToken;
+    res.status(201).json({token});
 });
 
 module.exports = router;
