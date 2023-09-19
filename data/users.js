8b2454de1d4b365c0339d4176c63c0be7afd7e34
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-19 01:50:03 -0500

    users list

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const users = [
+  { username: "fernando", email: "fernandolhoyosh@gmail.com", rol: "admin" },
+  { username: "adaschool", email: "adalovelace@adaschool.org", rol: "user" },
+  { username: "gunsnroses", email: "gnr@rock.us", rol: "user" },
+  { username: "administrator", email: "admin@developer.net", rol: "admin" },
+];
+
+module.exports = users;
