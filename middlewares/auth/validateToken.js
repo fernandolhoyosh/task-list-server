9f152b22c966cecb9050108b4ac261d7e8f229c4
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-09-19 21:03:39 -0500

    validate token JWT

--- middlewares/auth/validateToken.js
+++ middlewares/auth/validateToken.js
@@ -1,8 +1,25 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const validateToken = (req, res, next) => {
+const SECRET_KEY = process.env.SECRET_KEY;
 
+const validateToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({error:"Token no proporcionado"});
+    }
+    try {
+        const tokenDecoded = jwt.verify(token, SECRET_KEY);
+    
+        if (tokenDecoded.rol === 'admin' || tokenDecoded.rol === 'user') {
+          req.headers = {...req.headers, rol: tokenDecoded.rol}
+        }else {
+          return res.status(401).json({ error: "Rol no válido" });
+        }
+        next();
+      } catch (error) {
+        return res.status(401).json({error:"Token Invalido"});
+      }
 }
 
 module.exports = {validateToken}
--- routers/auth-router.js
+++ routers/auth-router.js
@@ -3,10 +3,21 @@ const router = express.Router();
 
 const users = require('../data/users');
 const {loginUser} = require('../middlewares/auth/loginUser');
+const {validateToken} = require('../middlewares/auth/validateToken');
 
 router.post('/login', loginUser, (req, res) => {
     const token = req.accessToken;
     res.status(201).json({token});
 });
+
+router.get('/ruta-protegida', validateToken, (req, res) => {
+    const userRol = req.headers.rol;
+  
+    if (userRol === 'admin') {
+      res.status(200).send("ACCESO CORRECTO");
+    } else {
+      res.status(401).json({ error: "Access not allowed" });
+    }
+  });
 
 module.exports = router;
